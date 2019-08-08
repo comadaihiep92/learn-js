df541d6c75be3e26f37022551d3635dadc04be0a
Stork <motden100@gmail.com>
2019-08-08 15:25:29 +0700

    Add bai 4

---
+++ bai-4-data-type/index.js
@@ -0,0 +1,44 @@
+// bai 4
+
+// number 
+var a1 = -100;
+var a2 = 0.5;
+
+// string
+var str = 'Hello, world.';
+var str2 = "Hello, world";
+console.log(str2);
+
+// dùng ngoặc đơn được ưa thích hơn, dùng ngoặc kép hơi rối mắt
+
+var str3 = 'Hello, I\'m Lan.';
+console.log(str3);
+// escape a character
+
+// \t Insert a tab in the text at this point.
+// \b Insert a backspace in the text at this point.
+// \n Insert a newline in the text at this point.
+// \r Insert a carriage return in the text at this point.
+// \f Insert a formfeed in the text at this point.
+// \' Insert a single quote character in the text at this point.
+// \" Insert a double quote character in the text at this point.
+// \\ Insert a backslash character in the text at this point.
+
+// Boolean (true, false) // dùng cho trạng thái của đối tượng trong lập trình
+var isHidden = true;
+console.log(isHidden);
+
+// undefined // khai báo mà chưa được gán giá trị gì
+var a;
+a = 100; // var a = 100;
+console.log(a);
+
+// null // giá trị rỗng không có gì bên trong // chưa được gán vào biến
+a = null;
+console.log(a);
+
+
+
+
+
+
