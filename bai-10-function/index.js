aea3c41d2d5be8895e5deb27d19a31ee575375ca
Stork <motden100@gmail.com>
2019-08-08 17:22:47 +0700

    Finish bai 10 function

--- bai-10-function/index.js
+++ bai-10-function/index.js
@@ -14,9 +14,33 @@ function caculateTriangleSquare(a, h) {
 // h = 5, cạnh đáy a = 10
 console.log(caculateTriangleSquare(10, 5));
 
+// Tính diện tích hình tròn
+// S = r * r * 3.14;
+// Tính diện tích hình tròn bán kính r = 10
 
+function calculateDiscSquare(r) {
+    return r * r * 3.14;
+};
 
+console.log(calculateDiscSquare(10));
+console.log(calculateDiscSquare(15));
+console.log(calculateDiscSquare(20));
 
+// Viết function tính diện tích hình vuông
+// Viết function tính diện tích hình chữ nhật
+// Viết function tính chu vi hình tròn
+// Thử tính diện tích/ chu vi các hình này
+// với tham số tùy bạn chọn
 
+function dtHinhVuong (a) {
+    return a * a;
+};
+
+console.log(dtHinhVuong(10));
+
+function dtHCN(a, b) {
+    return a * b;
+};
+console.log(dtHCN(10, 5));
 
 
