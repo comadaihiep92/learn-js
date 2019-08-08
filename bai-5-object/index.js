46a6379418f8672e687d6ef65c58ebfecc6ec21e
Stork <motden100@gmail.com>
2019-08-08 15:47:52 +0700

    Finish bai 5

--- bai-5-object/index.js
+++ bai-5-object/index.js
@@ -1,1 +1,34 @@
-//
+// object // được dùng để mô tả một đối tượng có thuộc tính nào đó // vd: con người có màu tóc, màu da ...
+
+// khai báo
+// var a = {
+//     key: value,  // key: thuộc tính - value: giá trị
+//     key: value
+// };
+var myDog = {
+    name: 'Lợn',
+    weight: 5.2,
+    isAlive: false
+};
+// myDog.weight = 10; // thay đổi thuộc tính cân nặng của con chó
+// console.log(myDog.name);
+
+myDog['isAlive'] = true;
+console.log(myDog);
+// console.log(myDog['name']); // myDog.name // [' truyen vao 1 string']
+
+var myBestFriend = {
+    nickName: 'Chó',
+    age: 28,
+    gender: 'male'
+};
+
+// camelCase (google)
+var a = {};
+
+// truy xuất dữ liệu của Object và thay đổi // tên biến . key
+
+
+
+
+
