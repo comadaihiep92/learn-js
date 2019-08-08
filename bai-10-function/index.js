33e61c1ce1db0af4bd5c04793f14ec60f031da7b
Stork <motden100@gmail.com>
2019-08-08 16:58:56 +0700

    Learn function bai 10

---
+++ bai-10-function/index.js
@@ -0,0 +1,22 @@
+// Function dùng để thực hiện một nhiệm vụ hay chức năng nào đó
+// từ khóa function // tên function // tên nhận đầu vào, cách nhau bởi dấu phẩy nếu có nhiều đầu vào
+// viết chức năng nào đó
+// trả về
+// khai báo được lưu vào bộ nhớ và được gọi để thực thi execute
+// gọi function // tên function // truyền giá trị đầu vào
+
+// làm việc với function có 2 bước // 1 khai báo // 2 gọi ra -- call
+function caculateTriangleSquare(a, h) {
+    return a * h /2;
+};
+
+// tính điện tích tam giác có chiều cao 
+// h = 5, cạnh đáy a = 10
+console.log(caculateTriangleSquare(10, 5));
+
+
+
+
+
+
+
