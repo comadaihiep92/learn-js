c0c90fcc3eb8e1b48822b0f21154d731b1e69323
Stork <motden100@gmail.com>
2019-08-09 09:16:48 +0700

    Finish bai 11 method of object

--- bai-11-object-methods/index.js
+++ bai-11-object-methods/index.js
@@ -9,9 +9,20 @@ var myDog = {
         // function ko có tên gọi là anonymous function // ko đặt tên cho function 
         console.log('Meo meo, my name is', this.name); // return undefined
         // function khi gán vào thuộc tính (property) của object gọi là method
+    },
+    eat: function(bone) {
+        this.weight = this.weight + bone.weight;
+        return this;
     }
 };
 console.log(myDog.bark());
+
+var bone = { weight: 0.5 };
+console.log('Before eating: ', myDog.weight);
+myDog.eat(bone);
+console.log('After eating: ', myDog.weight);
+
+
 
 // function bark() {
 //     console.log('Ụt ịt..');
@@ -20,7 +31,7 @@ console.log(myDog.bark());
 // console.log	là object và method, method thì truyền vào bao nhiêu cái input cũng dc, 1 input, 1 parameter
 // this	là keywork dùng trong ngữ cảnh (context) của method và nó tượng trưng cho object nó thuộc về
 
-function bark() {
-    console.log('Meo meo, my name is', this.name);
-};
-bark();
+// function bark() {
+//     console.log('Meo meo, my name is', this.name); // this là undefined vì ở global ko có thằng name
+// };
+// bark();
