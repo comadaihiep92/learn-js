9be91c3cffe2270a30a89551501c2484bffe3366
Stork <motden100@gmail.com>
2019-08-10 14:44:47 +0700

    Finish array.reduce

---
+++ bai-20-array.reduce/index.js
@@ -0,0 +1,29 @@
+// array.reduce
+/**
+ * arr.reduce(function(item1, item2) {
+ * // calculation
+ * return x;
+ * });
+ */
+
+ var numbers = [0, 1, 2, ,3 ,4];
+ var sum = numbers.reduce(function(a, b) {
+    console.log(a, b);
+    return a + b;  
+ });
+console.log(sum);
+console.log('--------');
+// giỏ hàng
+var orders = [
+    { name: 'A', quantity: 2, unitPrice: 100 },
+    { name: 'B', quantity: 1, unitPrice: 400 },
+    { name: 'C', quantity: 5, unitPrice: 15 }
+];
+
+var sumOrder = orders.reduce(function(x, y) {
+    console.log(y.quantity, y.unitPrice);
+    return x + (y.quantity * y.unitPrice);
+}, 0);
+console.log(sumOrder);
+
+
